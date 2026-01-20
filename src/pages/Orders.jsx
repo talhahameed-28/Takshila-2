9d1841dc841659a72060298761bbe6bededd8a86
ManavSharma <manavsharma@takshila.co>
2026-01-20 21:08:37 +0530

    order tracking responsiveness

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -94,31 +94,53 @@ export default function Orders() {
            
             {orders.map(order=>(
               <div key={order.id}>
-              <div   className="w-full cursor-pointer hover:scale-102 transition-all duration-200 hover:bg-[#7e7e7e] bg-[#d8d8d8] rounded-2xl p-5 flex items-center justify-between shadow-sm" onClick={() => {console.log(order);selectedOrder?.id==order.id?setSelectedOrder(null):setSelectedOrder(order)}}>
-              {/* Left Side */}
-              <div className="flex items-start space-x-4">
-                <div className="w-16 h-16 bg-white rounded-xl">
-                  <img className="w-full h-full rounded-xl object-cover" src={order.product.image} alt="order-image" />
+              <div
+                className="w-full cursor-pointer hover:scale-102 transition-all duration-200 hover:bg-[#7e7e7e] bg-[#d8d8d8] rounded-2xl px-1 py-2 md:p-5 flex items-center justify-between shadow-sm"
+                onClick={() => {
+                  selectedOrder?.id == order.id
+                    ? setSelectedOrder(null)
+                    : setSelectedOrder(order);
+                }}
+              >
+                {/* Left Side */}
+                <div className="flex items-start space-x-2 min-w-0 flex-1"> {/* ✅ */}
+                  <div className="w-16 h-16 bg-white rounded-xl shrink-0"> {/* ✅ */}
+                    <img
+                      className="w-full h-full rounded-xl object-cover"
+                      src={order.product.image}
+                      alt="order-image"
+                    />
+                  </div>
+
+                  <div className="flex flex-col text-sm min-w-0"> {/* ✅ */}
+                    <div className="text-xs sm:text-base break-words whitespace-normal font-semibold"> {/* ✅ */}
+                      {order.product.name}
+                    </div>
+
+                    <div className="text-xs sm:text-base w-fit text-gray-600">
+                      Ordered on : {order.created_at.split(" ")[0]}
+                    </div>
+
+                    <div className="text-xs sm:text-base w-fit text-gray-600">
+                      Order id : {order.order_number}
+                    </div>
+                  </div>
                 </div>
 
-                <div className="flex flex-col text-sm">
-                  <span className="font-semibold"> {order.product.name} </span>
-                  <span className="text-gray-600">Ordered on : {order.created_at.split(" ")[0]} </span>
-                  <span className="text-gray-600">Order id : {order.order_number} </span>
+                {/* Right Side */}
+                <div className="text-xs md:text-base flex flex-col text-right shrink-0"> {/* ✅ */}
+                  <span className="font-semibold text-left">
+                    ${order.amount}
+                  </span>
+                  <span className="mt-1 text-left sm:text-end text-gray-600 capitalize">
+                    {order.payment_status}
+                  </span>
                 </div>
               </div>
-
-              {/* Right Side */}
-              <div className="flex flex-col text-right text-sm">
-                <span className="font-semibold">${order.amount} </span>
-                <span className="mt-1 text-gray-600 capitalize">{order.payment_status} </span>
-              </div>
-                
-            </div>
              {/* {(selectedOrder && selectedOrder.id==order.id) && */}
             {(selectedOrder && selectedOrder.id==order.id) && (
               <div className="pb-3 border-bottom mb-4 border-dark">
-                            <table className="table track-table bg-transparent w-full m-auto max-w-[780px] font-montserrat text-gray-600">
+                            <table className="table track-table p-2 bg-transparent w-full m-auto max-w-[780px] font-montserrat text-gray-600">
                                 <tbody>
                                   
                                     <tr>
@@ -130,7 +152,7 @@ export default function Orders() {
                                         <td> {selectedOrder.progress[0].is_completed?`Completed on :  ${selectedOrder.progress[0].completed_at.split("T")[0]}`:"Pending..."} </td>
                                         <td>  <button className="underline decoration-1 hover:text-zinc-900" command="show-modal" commandfor="viewdetails"> View Details</button> </td>
                                         <td> 
-                                          {selectedOrder.progress[0].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
+                                          {selectedOrder.progress[0].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
                                            :<span className="no-fill"></span>}            
                                         </td>
                                     </tr>
@@ -140,11 +162,11 @@ export default function Orders() {
                                             CAD approved 
                                             </div>
                                          </td>
-                                        <td> {selectedOrder.progress[1].is_completed && selectedOrder.progress[1].completed_at!=null ?`Completed on :  ${selectedOrder.progress[1]?. completed_at.split("T")[0]}`:"Pending..."} </td>
+                                        <td > {selectedOrder.progress[1].is_completed && selectedOrder.progress[1].completed_at!=null ?`Completed on :  ${selectedOrder.progress[1]?. completed_at.split("T")[0]}`:"Pending..."} </td>
                                         <td> {selectedOrder.progress[1].started_at!=null?<button className="underline decoration-1 hover:text-zinc-900" onClick={()=>setModalStage("cad_approved")}> View CAD </button>:""} </td>
 
                                         <td> 
-                                          {selectedOrder.progress[1].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
+                                          {selectedOrder.progress[1].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
                                            :<span className="no-fill"></span>}            
                                         </td>
                                     </tr>
@@ -157,7 +179,7 @@ export default function Orders() {
                                         <td> {selectedOrder.progress[2].is_completed  && selectedOrder.progress[2].completed_at!=null?`Completed on :  ${selectedOrder.progress[2].completed_at.split("T")[0]}`:"Pending..."} </td>
                                         <td>{selectedOrder.progress[2].is_completed?<button onClick={()=>setModalStage("diamond_sourced")} className="underline decoration-1 hover:text-zinc-900" command="show-modal" commandfor="diamondsource">  View Diamond </button>:"" }</td>
                                         <td> 
-                                          {selectedOrder.progress[2].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
+                                          {selectedOrder.progress[2].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
                                            :<span className="no-fill"></span>}            
                                         </td>
                                       </tr>
@@ -170,7 +192,7 @@ export default function Orders() {
                                         <td> {selectedOrder.progress[3].is_completed && selectedOrder.progress[3].completed_at!=null?`Completed on :  ${selectedOrder.progress[3].completed_at.split("T")[0]}`:"Pending..."} </td>
                                         <td>{selectedOrder.progress[3].is_completed?<button onClick={()=>setModalStage("ring_in_production")} className="underline decoration-1 hover:text-zinc-900" command="show-modal" commandfor="ringstatus">  View Info</button>:""}</td>
                                         <td> 
-                                          {selectedOrder.progress[3].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
+                                          {selectedOrder.progress[3].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
                                            :<span className="no-fill"></span>}            
                                         </td>
                                     </tr>
@@ -181,7 +203,7 @@ export default function Orders() {
                                         <td> {selectedOrder.progress[4].is_completed && selectedOrder.progress[4].completed_at!=null?`Completed on :  ${selectedOrder.progress[4].completed_at.split("T")[0]}`:"Pending..."} </td>
                                         <td>{selectedOrder.progress[4].is_completed?<button onClick={()=>setModalStage("certification")} className="underline decoration-1 hover:text-zinc-900" command="show-modal" commandfor="certification">  View Cert </button>:""} </td>
                                         <td> 
-                                          {selectedOrder.progress[4].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
+                                          {selectedOrder.progress[4].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
                                            :<span className="no-fill"></span>}            
                                         </td>
                                     </tr>
@@ -192,7 +214,7 @@ export default function Orders() {
                                         <td> {selectedOrder.progress[5].is_completed && selectedOrder.progress[5].completed_at!=null?`Completed on :  ${selectedOrder.progress[5].completed_at.split("T")[0]}`:"Pending..."} </td>
                                         <td>{selectedOrder.progress[5].is_completed?<button onClick={()=>setModalStage("shipping")} className="underline decoration-1 hover:text-zinc-900" data-order-id="18" data-order='' command="show-modal" commandfor="shipping">  View Address </button>:""}</td>
                                         <td> 
-                                          {selectedOrder.progress[5].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
+                                          {selectedOrder.progress[5].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
                                            :<span className="no-fill"></span>}            
                                         </td>
                                     </tr>
@@ -203,7 +225,7 @@ export default function Orders() {
                                         <td> {selectedOrder.progress[6].is_completed  && selectedOrder.progress[6].completed_at!=null?`Completed on :  ${selectedOrder.progress[6].completed_at.split("T")[0]}`:"Pending..."} </td>
                                         <td>{ selectedOrder.progress[6].is_completed?<button onClick={()=>setModalStage("delivered")} className="underline decoration-1 hover:text-zinc-900" command="show-modal" commandfor="delivered">  View Address </button>:""} <button onClick={()=>setModalStage("delivered")} className="underline decoration-1 hover:text-zinc-900" command="show-modal" commandfor="delivered">  View Address </button> </td>
                                          <td> 
-                                          {selectedOrder.progress[6].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
+                                          {selectedOrder.progress[6].is_completed?<span className="fill-check"><svg xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></span> 
                                            :<span className="no-fill"></span>}            
                                         </td>
                                     </tr>
@@ -359,10 +381,10 @@ export default function Orders() {
         <dialog id="viewdetails" aria-labelledby="dialog-title" className="fixed inset-0 size-auto max-h-none max-w-4xl overflow-y-auto bg-transparent backdrop:bg-transparent mx-auto">
           <div className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"></div>
 
-          <div tabIndex="0" className="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
-            <div className="relative transform overflow-hidden -lg bg-[#716F6DE0] text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-2lg data-closed:sm:translate-y-0 data-closed:sm:scale-95 border rounded-xl border-gray-300">
+          <div tabIndex="0" className="flex min-h-full items-center justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
+            <div className="relative h-fit transform overflow-hidden -lg bg-[#716F6DE0] text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-2lg data-closed:sm:translate-y-0 data-closed:sm:scale-95 border rounded-xl border-gray-300">
               <div className="px-6 pt-9 pb-6 sm:p-6 sm:pb-4">
-                            <div className="col-md-12 details-generate text-start md:px-12 px-4 pt-5">
+                            <div className="col-md-12 details-generate text-start md:px-12 pt-5">
                                 <h2 className="text-center text-2xl font-bold uppercase text-white pb-5"> Order Details </h2>
                                 <div className="flex flex-row mt-8 w-full justify-center">
                                     <div className="basis-1/2">
@@ -377,12 +399,12 @@ export default function Orders() {
                                 <p className="text-white"> Payment verified and your build slot is reserved. </p>
 
 
-                                <div className="flex flex-row mt-7 w-full">
+                                <div className="flex flex-col sm:flex-row mt-7 w-full">
                                     <div className="basis-1/2">
                                         <h5 className="mb-3 text-white uppercase font-semibold"> ORDER CONFIRMED </h5>
                                     </div>
-                                    <div className="basis-1/2 text-end">
-                                        <h5 className="text-white mb-0">Order id: <strong className="block"> {selectedOrder?.order_number}</strong></h5>
+                                    <div className="basis-1/2 shrink-0 wrap-break-word whitespace-normal sm:text-end">
+                                        <h5 className="text-white mb-0 text-wrap">Order id: <strong className="block"> {selectedOrder?.order_number}</strong></h5>
                                     </div>
                                 </div>
                                 <h6 className="mb-3 text-white text-start"> Artisan Assigned </h6>
